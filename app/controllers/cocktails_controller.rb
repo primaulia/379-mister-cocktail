ea1b4e47c200c193331b398abf091449577450e7
Prima Aulia Gusta <primAulia@gmail.com>
2020-05-25 17:45:04 +0800

    latest with 4 first features

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,4 +2,29 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def show
+    @the_cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @new_cocktail = Cocktail.new
+  end
+
+  def create
+    @new_cocktail = Cocktail.new(cocktail_params)
+    
+    if @new_cocktail.save
+      redirect_to @new_cocktail
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
