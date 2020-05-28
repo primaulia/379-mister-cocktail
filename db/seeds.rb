0b30d5d054a31f584bd8b25a0d83c4cd4c9aefff
Prima Aulia Gusta <primAulia@gmail.com>
2020-05-28 19:57:39 +0800

    setup with updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
 
 puts "Setup DB"
-Ingredient.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
 
 puts "Create 100 Ingredients by thecocktaildb API"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
@@ -17,10 +17,20 @@ end
 
 puts "Create 5 seeded cocktails"
 
-Cocktail.create!(name: 'Mojito')
-Cocktail.create!(name: 'Bloody Mary')
-Cocktail.create!(name: 'Sex on the Beach')
-Cocktail.create!(name: 'Margarita')
-Cocktail.create!(name: 'Pina Colada')
+cocktails_arr = ['Mojito', 'Bloody Mary', 'Sex on the Beach', 'Margarita', 'Pina Colada']
+
+cocktails_arr.each do |cocktail|
+  puts "Create #{cocktail} with a photo"
+  the_cocktail = Cocktail.create!(name: cocktail)
+  cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{the_cocktail.name}"
+
+  cocktail_json = open(cocktail_url).read
+  cocktail_data = JSON.parse(cocktail_json)
+
+  thumbnail_url = cocktail_data["drinks"][0]["strDrinkThumb"]
+
+  file = URI.open(thumbnail_url) # downloading to the memory
+  the_cocktail.photo.attach(io: file, filename: "#{the_cocktail.name}.jpg", content_type: 'image/jpg')
+end
 
 puts "End of seeds"
